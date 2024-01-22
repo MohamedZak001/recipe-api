3acf2e012460f1e5a06b4851e9efe9e30e673b87
Mohamed Zakaria <medozakaria014@gmail.com>
2024-01-22 19:58:13 +0200

    add nginx service and configur uwsgi server with django

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g&%6vk1dt%_4k!_#hge$*(ul4m6ml!s)e&tvzrdm-*w(0$7(m$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 # Application definition
 
@@ -84,12 +90,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER':os.environ.get('DB_USERNAME') ,
-        'PASSWORD':os.environ.get('DB_PASSWORD'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
     }
 }
-
 
 
 # Password validation
